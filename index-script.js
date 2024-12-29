72158aa5044c2a1e0bc6c2adb9fb6e44eb65e511
Alexander <lalekoslalekos@yahoo.com>
2024-12-29 23:17:45 +0200

    Updated

--- index-script.js
+++ index-script.js
@@ -1,10 +1,10 @@
 const translations = {
     "en": {
         "title": "DogSoftware - Indie Studio",
-        "description": "Indie studio based in 🇬🇷 Greece developing desktop games/applications and mods for existing games",
+        "description": "Indie studio based in 🇬🇷 Greece developing desktop games/applications",
         "team-title": "Meet the team!",
         "team-description": "Meet the masterminds behind DogSoftware:",
-        "Lalekos": "Mr.Lalekos",
+        "Lalekos": "Alektron",
         "contributors-title": "Meet the contributors!",
         "contributors-description": "Cool People:",
         "home-link": "Home",
@@ -14,25 +14,24 @@ const translations = {
         "lalekos-title": "Programmer",
         "Spyridon": "Spyridon",
         "spyridon-title": "Idea Guy, Voice Actor",
-        "srabb-title": "SFX Artist",
+        "srabb-title": "Music&SFX Artist",
         "EmailInfo": "Feel free to contact us via email for inquiries, support, or any questions you may have.",
         "Question":"Wanna get in touch?",
         "WebApps":"WebApps-OldestToNewest",
         "Games":"Games-OldestToNewest",
         "noble-title": "Programmer",
         "rocky-title": "Voice Actor",
-        "joel-title": "Music Composer",
         "Jam2":"Cool and satisfying game in 16 hours? #2 - Winners",
-        "rust-title":"Arist",
+        "rust-title":"Artist",
         "AboutDS-Title": "About DogSoftware",
-        "About-desc":"DogSoftware was founded back in July of 2021 by Mr. Alexandros (AKA Alekos, Alexander, Lalekos) and evolved from basically solo projects to a ''family team thing'' with Henry, Joel, Srabb and Rust. Right now working on The Mayhem Playground constantly."
+        "About-desc":"DogSoftware was founded back in July of 2021 by Alektron and evolved from basically solo projects to a ''family team thing'' with Henry, Srabb and Rust. Right now working on the ''modern'' version of MAD2 (by the way we call it The Mayhem Playground) but mostly ''inspired'' from Mestiez's People Playground."
     },
     "el": {
         "title": "DogSoftware - Ανεξάρτητο Στούντιο",
-        "description": "Ανεξάρτητο στούντιο με έδρα την 🇬🇷 Ελλάδα που αναπτύσσει παιχνίδια/εφαρμογές για υπολογιστές και mods για υπάρχοντα παιχνίδια",
+        "description": "Ανεξάρτητο στούντιο με έδρα την 🇬🇷 Ελλάδα που αναπτύσσει παιχνίδια/εφαρμογές για υπολογιστές",
         "team-title": "Γνωρίστε την ομάδα!",
         "team-description": "Γνωρίστε τους εγκέφαλους πίσω από την DogSoftware:",
-        "Lalekos": "Κυρ-Λαλέκος",
+        "Lalekos": "Κυρ-Αλέξανδρος",
         "contributors-title": "Γνωρίστε τους συνεισφέροντες!",
         "contributors-description": "Γαμάτοι Άνθρωποι:",
         "home-link": "Αρχική",
@@ -42,18 +41,17 @@ const translations = {
         "lalekos-title": "Προγραμματιστής",
         "Spyridon": "Σπυρίδον",
         "spyridon-title": "Ιδέες, Ηθοποιός Φωνής",
-        "srabb-title": "Σχεδιαστής Ηχητικών Εφέ",
+        "srabb-title": "Μουσική&SFX Σχεδιαστής",
         "EmailInfo": "Μη διστάσετε να επικοινωνήσετε μαζί μας μέσω email για ερωτήσεις, υποστήριξη ή οποιαδήποτε άλλη ερώτηση που μπορεί να έχετε!",
         "Question":"Θέλετε να επικοινωνήσετε;",
         "WebApps":"WebApps-ΑπόΠαλαιότεροΣεΝεώτερο",
         "Games":"Παιχνίδια-ΑπόΠαλαιότεροΣεΝεώτερο",
         "noble-title": "Προγραμματιστής",
         "rocky-title": "Ηθοποιός Φωνής",
-        "joel-title": "Μουσικοσυνθέτης",
         "Jam2":"Γαμάτο και satisfying παιχνίδι σε 16 ώρες; #2 - Νικητές",
         "rust-title":"Καλλιτέχνης",
         "AboutDS-Title": "Σχετικά με την DS",
-        "About-desc":"Η DogSoftware ιδρύθηκε τον Ιούλιο του 2021 από τον Κυρ Αλέξανδρο (γνωστός επίσης ως Αλέκος, Λαλέκος και στα Εγγλέζικα Alexander) και το στούντιο γενικότερα εξελίχθηκε με απλά λόγια από solo έργα σε μια «οικογενειακή ομάδα» με τους Henry, Joel, Srabb και Rust. Αυτή τη στιγμή εργάζονται συνεχώς πάνω στο The Mayhem Playground."
+        "About-desc":"Η DogSoftware ιδρύθηκε τον Ιούλιο του 2021 από τον Κυρ-Αλέξανδρο και το στούντιο γενικότερα εξελίχθηκε με απλά λόγια από solo έργα σε μια «οικογενειακή ομάδα» με τους Henry, Srabb και Rust. Αυτή τη στιγμή δουλεύουμε πάνω στη «μοντέρνα» έκδοση του MAD2 (παρεμπιπτόντως το αποκαλούμε The Mayhem Playground), αλλά κυρίως είμαστε <<εμπνευσμένοι>> από το People Playground του Mestiez."
     }
 };
 
